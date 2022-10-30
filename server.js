dab5f5b559886503087abe3ea06071914de2a9e8
Salman Dabbakuti <dabbakutisalman@gmail.com>
2022-10-30 13:46:00 +0530

    updated: random number generation

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-const http = require('http');
+const http = require("http");
 
 const requestListener = (req, res) => {
-  if (req.url === '/sse') {
-    res.statusCode = 200;
+  if (req.url === "/sse") {
     res.writeHead(200, {
-      'Access-Control-Allow-Origin': '*',
-      'Content-Type': 'text/event-stream',
-      'Cache-Control': 'no-cache',
-      'Connection': 'keep-alive'
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "text/event-stream",
+      "Cache-Control": "no-cache",
+      Connection: "keep-alive"
     });
     setInterval(() => {
       const timestamp = new Date().toLocaleTimeString();
-      const ticker = Math.random();
-      console.log('ticking..');
-      res.write(`data: ${timestamp}: ${ticker}\n\n`);
+      // random number between 0 and 1000
+      const ticker = Math.floor(Math.random() * 1000);
+      console.log("ticking..");
+      res.write(`data: ${ticker}\n\n`);
       console.log(`${timestamp}: ${ticker}`);
     }, 1000);
   } else {
@@ -24,6 +24,6 @@ const requestListener = (req, res) => {
 
 const server = http.createServer(requestListener);
 
-server.listen(4000, () => {
-  console.log("server running at http://localhost:4000");
-});
+server.listen(4000, () =>
+  console.log("server listening at http://localhost:4000")
+);
