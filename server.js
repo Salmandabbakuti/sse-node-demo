edef92d99cd70a6366a7a17bbd0e89ddee9348f1
Salman Dabbakuti <dabbakutisalman@gmail.com>
2022-11-16 22:56:49 +0530

    added: graphql sse ticker

--- server.js
+++ server.js
@@ -1,29 +1,47 @@
-const http = require("http");
+const { createServer } = require("http");
+const { createSchema, createYoga } = require('graphql-yoga');
 
-const requestListener = (req, res) => {
-  if (req.url === "/sse") {
-    res.writeHead(200, {
-      "Access-Control-Allow-Origin": "*",
-      "Content-Type": "text/event-stream",
-      "Cache-Control": "no-cache",
-      Connection: "keep-alive"
-    });
-    setInterval(() => {
-      const timestamp = new Date().toLocaleTimeString();
-      // random number between 0 and 1000
-      const ticker = Math.floor(Math.random() * 1000);
-      console.log("ticking..");
-      res.write(`data: ${ticker}\n\n`);
-      console.log(`${timestamp}: ${ticker}`);
-    }, 1000);
-  } else {
-    res.writeHead(200);
-    res.end('Hello. This is an SSE server. Please go to /sse');
+const schema = createSchema({
+  typeDefs: /* GraphQL */ `
+    type Query {
+      hello: String
+    }
+    type Mutation {
+      hello: String
+    }
+    type Subscription {
+      ticker: Int
+    }
+  `,
+  resolvers: {
+    Query: {
+      hello: () => 'Hello GraphQL!'
+    },
+    Mutation: {
+      hello: () => 'Hello GraphQL!'
+    },
+    Subscription: {
+      ticker: {
+        subscribe: async function* (root, args, ctx, info) {
+          while (true) {
+            await new Promise(resolve => setTimeout(resolve, 1000));
+            yield { ticker: Math.floor(Math.random() * 1000) };
+          }
+        }
+      }
+    }
   }
-};
+});
 
-const server = http.createServer(requestListener);
+const yoga = createYoga({
+  schema,
+  maskedErrors: false,
+  graphiql: true,
+  graphqlEndpoint: '/graphql',
+  landingPage: true,
+  plugins: []
+});
 
-server.listen(4000, () =>
-  console.log("server listening at http://localhost:4000")
-);
+const server = createServer(yoga);
+
+server.listen(4000, () => console.log("Server started on http://localhost:4000"));
